2a671ec8c96e61ef7cb0034bbf702e67e5c42920
Shilpita Biswas <51776663+sh-biswas@users.noreply.github.com>
2020-08-08 16:04:39 -0700

    Redone with firebase

--- app.py
+++ app.py
@@ -1,78 +1,54 @@
-# Import Flask Library
-from flask import Flask, render_template, request, session, url_for, redirect
-import pymysql.cursors
-import datetime
-from dateutil import relativedelta
-import random
+import pyrebase
+from flask import *
 
-# Initialize the app from Flask
 app = Flask(__name__)
 
-# Configure MySQL
-conn = pymysql.connect(host='localhost',
-                       user='root',
-                       password='something',
-                       db='something',
-                       charset='utf8mb4',
-                       cursorclass=pymysql.cursors.DictCursor)
+config = {
+    "apiKey": "AIzaSyDQkaQjhaa0ueRT_D-1vVtXMbX8xtqPwBI",
+    "authDomain": "covid-19-isolation-tracker.firebaseapp.com",
+    "databaseURL": "https://covid-19-isolation-tracker.firebaseio.com",
+    "projectId": "covid-19-isolation-tracker",
+    "storageBucket": "covid-19-isolation-tracker.appspot.com",
+    "messagingSenderId": "435855688991",
+    "appId": "1:435855688991:web:17bcb9b15f945d4cc11c2a",
+    "measurementId": "G-JBSNN6Z8DV"
+}
 
-#====================== APP =======================
-#-------------------- HOME? ----------------------
-@app.route('/')
+firebase = pyrebase.initialize_app(config)
+
+auth = firebase.auth()
+
+@app.route('/', methods = ['GET', 'POST'])
 def public():
-    loggedin=None
-    if session != {}:
-        loggedin=True
-    return render_template('index.html', loggedin=loggedin)
+    return render_template('index.html')
 
-#-----------------LOGIN & authentication---------------
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 @app.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
-    # grabs information from the forms
-    username = request.form['username']
-    password = request.form['password']
-
-    # cursor used to send queries
-    cursor = conn.cursor()
-    # executes query
-    query = 'SELECT * FROM users WHERE username = %s and password = md5(%s)'
-    cursor.execute(query, (username, password))
-    # stores the results in a variable
-    data = cursor.fetchone()
-    # use fetchall() if you are expecting more than 1 data row
-    cursor.close()
-    error = None
-    if (data):
-        # creates a session for the the user
-        # session is a built in dictionary
-        session['username'] = username
-        return redirect(url_for('home'))
-    else:
-        # returns an error message to the html page
-        error = 'Invalid login or username'
-        return render_template('login.html', error=error)  # send the error msg to html
-
-
-#------------------- LOGOUT --------------------
-@app.route('/logout',methods=['GET','POST'])
-def logout():
-    if session.get("storedData") is not None:
-        session.pop("storedData")
-    session.pop('username')
-    return render_template("logout.html")
+    if request.method == 'POST':
+        email = request.form['name']
+        password = request.form['pass']
+        try:
+            auth.sign_in_with_email_and_password('email','password')
+            return 'Login is successful'
+        except:
+            error = 'Invalid login or username'
+            return render_template('login.html', error=error)
 
 
 
-#======================== RUN APP ============================
-app.secret_key = 'some key that you will never guess'
-# Run the app on localhost port 5000
-# debug = True -> you don't have to restart flask
-# for changes to go through, TURN OFF FOR PRODUCTION
+#email = input('Please enter your email\n')
+#password = input('Please enter your password\n')
 
-if __name__ == "__main__":
-    app.run('127.0.0.1', 5000, debug=True)
+#user = auth.create_user_with_email_and_password(email, password)
 
+#user = auth.sign_in_with_email_and_password(email, password)
+
+#auth.get_account_info(user['idToken'])
+
+if __name__ =='__main__':
+    app.run(debug=True)
