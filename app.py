0c0859e7041308ca7a36724cde0195f8cd8783bd
Shilpita Biswas <51776663+sh-biswas@users.noreply.github.com>
2020-08-09 08:48:04 -0700

    Removed dashboard

--- app.py
+++ app.py
@@ -219,43 +219,6 @@ def logout():
     return render_template('index.html')
 
 
-@app.route('/dashboard', methods=['GET','POST'])
-def cal():
-    creds = None
-    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with  open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-    'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            # with open('token.pickle', 'wb') as token: # can't write files in Google App Engine so comment out or delete
-            # pickle.dump(creds, token)
-    service = googleapiclient.discovery.build('calendar', 'v3', credentials=creds)
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() +  'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='find your cal id from google and paste it here', timeMin=now, maxResults=10, singleEvents=True, orderBy='startTime').execute()
-    events = events_result.get('items', [])
-    if not events:
-        print('No upcoming events found.')
-    # for event in events:
-    # start = event['start'].get('dateTime', event['start'].get('date'))
-    # print(start, event['summary'])
-    event_list = [event["summary"] for event in events]
-
-    return render_template('dashboard.html', events = events_list)
-
-
 #secret key
 app.secret_key = b'_5askq478eqrbkhdfs]/'
 
